023293bc36c09db809dcfba895f9136b6b6385f4
gness1804 <graham.nessler@gmail.com>
2022-05-13 14:27:08 -0500

    refactor

--- binary-search-tree.ts
+++ binary-search-tree.ts
@@ -1,7 +1,7 @@
 export class BinarySearchTree {
-  _data: number;
-  _left: BinarySearchTree | undefined;
-  _right: BinarySearchTree | undefined;
+  readonly _data: number;
+  private _left: BinarySearchTree | undefined;
+  private _right: BinarySearchTree | undefined;
   constructor(data: number) {
     this._data = data;
   }
@@ -20,28 +20,16 @@ export class BinarySearchTree {
 
   public insert(item: number) {
     if (item <= this._data) {
-      if (this._left) {
-        this._left.insert(item)
-      } else {
-        this._left = new BinarySearchTree(item);
-      }
+      this._left ? this._left.insert(item) : this._left = new BinarySearchTree(item);
     } else {
-      if (this._right) {
-        this._right.insert(item)
-      } else {
-        this._right = new BinarySearchTree(item);
-      }
+      this._right ? this._right.insert(item) : this._right = new BinarySearchTree(item);
     }
   }
 
   public each(callback: (data: number) => void) {
     // iterate through each child BinarySearchTree and get the data
-    if (this._left) {
-      this._left.each(callback);
-    }
+    this._left?.each(callback);
     callback(this._data);
-    if (this.right) {
-      this.right.each(callback);
-    }
+    this._right?.each(callback);
   }
 }
